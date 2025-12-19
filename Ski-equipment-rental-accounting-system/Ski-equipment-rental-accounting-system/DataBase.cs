bfd1c93ac96a9967eecd3ddf8824c6e008cd641a
TuBe3rKuL3z <nulevyjakkaunt0@gmail.com>
2025-12-19 10:16:39 +0300

    Добавлена AR Диаграмма

---
+++ Ski-equipment-rental-accounting-system/Ski-equipment-rental-accounting-system/DataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Ski_equipment_rental_accounting_system
+{
+    private class DataBase
+    {
+        static string connStr = "Messenger.db";
+     
+
+        private static void CreateDb()
+        {
+            SqliteConnection conn = new SqliteConnection();
+            
+
+            try
+            {
+                conn.Open();
+                SqliteCommand cmd = new SqliteConnection().CreateCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"CREATE TABLE user ();"
+            }
+        }
+
+    }
+
+    
+}
